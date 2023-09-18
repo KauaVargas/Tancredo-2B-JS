765e26bb237eb637334cb8a6f5147aff230f82e8
Kauã Vinícius <106679339+KauaVargas@users.noreply.github.com>
2023-09-18 10:38:08 -0300

    Aula 18/09

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-function tocaSomPom () {
-    document.querySelector ('#som_tecla_pom').play ();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-function tocaSomClap () {
-    document.querySelector ('#som_tecla_clap').play ();
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`;//template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector ('#som_tecla_tim').play ();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
